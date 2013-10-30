915c8b58f80af70fa4cf76113541b77f3400720a
Hermann Kaser <hermann.kaser@gmail.com>
2013-10-30 21:29:46 +0000

    Format

--- js/change_save_button.js
+++ js/change_save_button.js
@@ -1,16 +1,17 @@
 if (window.rcmail) {
-  rcmail.addEventListener('init', function(evt) {
-    rcmail.register_command('plugin.password-save-without-validation', function() {
-	    rcmail.gui_objects.passform.submit();
-    }, true);
-  })
+	rcmail.addEventListener('init', function(evt) {
+		rcmail.register_command('plugin.password-save-without-validation', function() {
+			rcmail.gui_objects.passform.submit();
+		}, true);
+	})
 }
 
-$(document).ready(function($){
+$(document).ready(function($)
+{
 	$('input.button.mainaction').remove();
-	$('.boxcontent').append('<p><input type="button" value="Salvar" id="save_button" class="button mainaction"></p>')
+	$('.boxcontent').append('<p><input type="button" value="Salvar" id="save_button" class="button mainaction"></p>');
 
-	$('#save_button').click(function(){
+	$('#save_button').click(function() {
 		rcmail.command('plugin.password-save-without-validation','',this);
 	});
 });
